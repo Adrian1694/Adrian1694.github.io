ac64751914b081dcdd917beeda0d32575faae38b
Adrian1694 <stelyady@yahoo.com>
2017-12-14 19:30:41 +0200

    Altitude

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.2";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.3";
 
 navigator.geolocation.getCurrentPosition(on_position_success, on_position_failure);
 
@@ -9,7 +9,8 @@ function on_position_success(e)
 	document.getElementById("id_lat").innerHTML = "Latitude = "+ e.coords.latitude;
 	document.getElementById("id_long").innerHTML = "Longitude = "+ e.coords.longitude;
 	document.getElementById("id_acc").innerHTML = "Accuracy = "+ e.coords.accuracy + "m";
-
+	document.getElementById("id_acc").innerHTML = "Altitude = "+ e.coords.altitude + "m";
+		
 
 }
 //.................................
