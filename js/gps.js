aa5667009f1c3d70deac3a3f2150f12697ffd9da
Adrian1694 <stelyady@yahoo.com>
2017-12-14 19:22:43 +0200

    Geo location

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.1";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.2";
 
 navigator.geolocation.getCurrentPosition(on_position_success, on_position_failure);
 
@@ -6,8 +6,9 @@ navigator.geolocation.getCurrentPosition(on_position_success, on_position_failur
 //......................................
 function on_position_success(e)
 {
-	document.getElementById("id_lat").innerHTML = e.coords.latitude;
-	document.getElementById("id_long").innerHTML = e.coords.longitude;
+	document.getElementById("id_lat").innerHTML = "Latitude = "+ e.coords.latitude;
+	document.getElementById("id_long").innerHTML = "Longitude = "+ e.coords.longitude;
+	document.getElementById("id_acc").innerHTML = "Accuracy = "+ e.coords.accuracy + "m";
 
 
 }
