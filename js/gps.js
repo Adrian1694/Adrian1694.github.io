6bbb794144d3f2fc6cec96bcb7876680571e4d11
Adrian1694 <stelyady@yahoo.com>
2018-01-08 17:58:46 +0200

    Maps GPS v7

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.5";
+document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.6";
 
 navigator.geolocation.getCurrentPosition(on_position_success, on_position_failure);
 
@@ -11,8 +11,8 @@ function on_position_success(e)
 	document.getElementById("id_acc").innerHTML = "Accuracy = "+ e.coords.accuracy + "m";
 	document.getElementById("id_alt").innerHTML = "Altitude = "+ e.coords.altitude + "m";
 	
-	var map_str = "https://maps.googleapis.com/maps/api/staticmap?center=" +
-					e.coords.latitude +","+ e.coords.longitude +
+	var map_str = "https://maps.googleapis.com/maps/api/staticmap?"+
+					"center="+ e.coords.latitude +","+ e.coords.longitude +
 					"&zoom=15" +
 					"&size=400x300" +
 					"&key=AIzaSyAjpdbOR6jJN1R2vDbST_vad43pVGnnNxg"
