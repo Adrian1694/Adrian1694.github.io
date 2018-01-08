d3415ec17e7e4ae62f1b01adc32ea9c2845ed29d
Adrian1694 <stelyady@yahoo.com>
2018-01-08 17:35:48 +0200

    GPS MAP

--- js/gps.js
+++ js/gps.js
@@ -9,8 +9,15 @@ function on_position_success(e)
 	document.getElementById("id_lat").innerHTML = "Latitude = "+ e.coords.latitude;
 	document.getElementById("id_long").innerHTML = "Longitude = "+ e.coords.longitude;
 	document.getElementById("id_acc").innerHTML = "Accuracy = "+ e.coords.accuracy + "m";
-	document.getElementById("id_acc").innerHTML = "Altitude = "+ e.coords.altitude + "m";
-		
+	document.getElementById("id_alt").innerHTML = "Altitude = "+ e.coords.altitude + "m";
+	
+	var map_str = "https://maps.googleapis.com/maps/api/staticmap?center=" +
+					e.coords.latitude + ", "+ e.coords.longitude +
+					"&zoom=15" +
+					"&size=400x300" +
+					"key=AIzaSyAjpdbOR6jJN1R2vDbST_vad43pVGnnNxg"
+					;
+	document.getElementById("id_img").src = map_str;
 
 }
 //.................................
@@ -21,3 +28,5 @@ function on_position_failure(e)
 
 }
 //..................................
+
+AIzaSyAjpdbOR6jJN1R2vDbST_vad43pVGnnNxg
