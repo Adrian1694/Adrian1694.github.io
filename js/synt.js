78d74e2ae7df0917216dc82142fc510276c97ef3
Adrian1694 <stelyady@yahoo.com>
2018-01-15 17:33:55 +0200

    Synt voices

--- js/synt.js
+++ js/synt.js
@@ -1,3 +1,30 @@
 var d = new Date ();
 document.getElementById("id_business_version").innerHTML = "Business version = " + 
 d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".0";
+
+document.addEventListener("touchstart", speak);
+
+var synt = window.speechSynthesis;
+//..............................................
+function on_get_voices()
+{
+	var voci = synt.getVoices();
+	for (var i = 0; i < voci.length; i++) {
+		document.getElementById("id_voices").innerHTML += voci[i].lang + " " + voci[i].name + "<br>";
+	
+	}
+	
+	
+	
+	
+}
+//........................................................
+function speak()
+{
+	var enunt = new SpeechSynthesisUtterance();
+	enunt.lang = "en-US";
+	enunt.text = document.getElementById ("id_text").value;
+	
+	synt.speak(enunt);
+}
+//.............................................................
