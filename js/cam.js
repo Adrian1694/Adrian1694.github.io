08baa1bb9c3d4c85cadf13dbec8e0ab1fb3fda06
Adrian1694 <stelyady@yahoo.com>
2018-01-22 18:32:44 +0200

    video cam canvas

--- js/cam.js
+++ js/cam.js
@@ -1,6 +1,6 @@
 var d = new Date ();
 document.getElementById("id_business_version").innerHTML = "Business version = " + 
-d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".5";
+d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".6";
 
 //...........................................
 
