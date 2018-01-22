3e1ba50a5871d4eea323342c010066881bb1513d
Adrian1694 <stelyady@yahoo.com>
2018-01-22 18:58:14 +0200

    vibrate

--- js/vibrate.js
+++ js/vibrate.js
@@ -3,7 +3,7 @@ document.getElementById("id_business_version").innerHTML = "Business version = "
 d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".7";
 
 var img = document.getElementById("id_img");
-img.addEventListener("touchstart", "on_touch_start");
+img.addEventListener("touchstart", on_touch_start);
 
 function on_touch_start(e)
 {
