6f8a788789c4bb222b8364e1d4d8afc64d9b34f1
Adrian1694 <stelyady@yahoo.com>
2018-01-22 19:32:38 +0200

    battery

--- js/battery.js
+++ js/battery.js
@@ -1,12 +1,12 @@
 var d = new Date ();
 document.getElementById("id_business_version").innerHTML = "Business version = " + 
-d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".0";
+d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".1";
 
 navigator.getBattery().then(on_battery).catch(on_battery_error);
 //............................................
 function on_battery(b)
 {
-	document.getElementById("id_battery").innerHTML = b.level;
+	document.getElementById("id_battery").innerHTML = b.level + " " + b.charging;
 }
 //............................................
 function on_battery_error()
