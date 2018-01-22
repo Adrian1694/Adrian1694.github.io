82e5d342933c876ef31d32c006636861cb451d61
Adrian1694 <stelyady@yahoo.com>
2018-01-22 18:59:36 +0200

    vibrate

--- js/vibrate.js
+++ js/vibrate.js
@@ -1,11 +1,11 @@
 var d = new Date ();
 document.getElementById("id_business_version").innerHTML = "Business version = " + 
-d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".7";
+d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".8";
 
 var img = document.getElementById("id_img");
 img.addEventListener("touchstart", on_touch_start);
 
 function on_touch_start(e)
 {
-	window.navigator.vibrate(200);
+	window.navigator.vibrate([100, 100, 200, 200, 300, 300]);
 }
