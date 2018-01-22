62588efb812a20826dfd617f3a031f1cda97b649
Adrian1694 <stelyady@yahoo.com>
2018-01-22 17:48:48 +0200

    video cam

---
+++ js/cam.js
@@ -0,0 +1,25 @@
+var d = new Date ();
+document.getElementById("id_business_version").innerHTML = "Business version = " + 
+d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".6";
+
+//...........................................
+
+var constraints = (audio: true, video: true);
+
+navigator.MediaDevices.getUserMedia(constraints)
+.then(on_cam())
+.catch(on_error());
+
+var video = document.getElementById("id_video");
+
+//............................................
+function on_cam(stream)
+{
+	video.src = stream;
+}
+//...........................................
+function on_error(e)
+{
+	alert("Error: cannot connect to camera!");
+}
+//...........................................
