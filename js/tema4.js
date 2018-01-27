35a54f66578e617d6f25afb651a0aa1df68ded72
Adrian1694 <stelyady@yahoo.com>
2018-01-27 16:06:33 +0200

    tema 4

--- js/tema4.js
+++ js/tema4.js
@@ -2,26 +2,14 @@ var d = new Date ();
 document.getElementById("id_business_version").innerHTML = "Business version = " + 
 d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".0";
 
-var img = document.getElementById("id_img");
-img.addEventListener("touchstart", on_touch_start);
-img.addEventListener("touchend", on_touch_end);
+var vid = document.getElementById("ktm_video");
 
 //...................................................
-function on_touch_start(e)
-{
-	e.preventDefault();
-	timer_id = setInterval(start_vibrate, 100);
-	
-}
-//...................................................
-function start_vibrate()
-{
-		window.navigator.vibrate(150);
-}
-//...................................................
-function on_touch_end()
-{
-		e.preventDefault();
-		clearInterval(timer_id);
-}
+function playVid() { 
+    vid.play(); 
+} 
+
+function pauseVid() { 
+    vid.pause(); 
+} 
 //...................................................
